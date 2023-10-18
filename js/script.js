b6a3e9e971f66085473efb84810571b1a89472bc
jody_d <jodydepp@gmail.com>
2023-10-18 13:36:05 -0400

    Made the button work and show a cute cat with the classlist.add

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
+//Create variables to grab .show-cat and .cat
 const button = document.querySelector(".show-cat");
-console.log(button);
+//console.log(button);
+const cat = document.querySelector(".cat");
+//console.log(cat);
+//create eventListener and handler for the button
+button.addEventListener("click", function () {
+    button.innerText = "It's working? - Yes";
+    //in the function body we use the car variable and and classlist property to add the show class
+    cat.classList.add("show");
+});
